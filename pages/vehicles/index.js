35987741a54386300d1a268e63cf9b5b15044503
kshymes <104524124+kshymes@users.noreply.github.com>
2023-12-12 18:29:28 -0500

    starting animation

--- pages/vehicles/index.js
+++ pages/vehicles/index.js
@@ -1,12 +1,9 @@
 import { useState } from 'react';
 
-
 import Container from '../../components/Container';
 import FilterBar from '../../components/FilterBar';
 import Grid from '../../components/Grid';
-import Image from 'next/image';
 import Layout from '../../components/Layout';
-import Link from 'next/link';
 
 import { getAllVehicles, getVehicleTypes } from '../../lib/api'
 
@@ -51,25 +48,10 @@ const vehiclesPage = ({ vehicles, vehicleTypes }) => {
             activeItem={activeVehicleType}
             setActiveItem={setActiveVehicleType}
             />
-            <Grid>
-                {filteredVehicles.map((vehicle, index) => {
-                const { title, slug, vehicleInformation } = vehicle.node;
-                const { trimLevels } = vehicleInformation;
-                return <article key={index}>
-                {trimLevels && trimLevels[0].images.thumbnail && 
-                <Image 
-                src={trimLevels[0].images.thumbnail.node.sourceUrl}
-                alt={trimLevels[0].images.thumbnail.node.altText}
-                width={trimLevels[0].images.thumbnail.node.mediaDetails.width}
-                height={trimLevels[0].images.thumbnail.node.mediaDetails.height}
-                />
-                }
-                <h3>{title}</h3>
-                <Link href={`/vehicles/${slug}`}>Learn More</Link>
-                </article>
-            })}
-        </Grid>
-    </Container>
-</Layout>
+            <Grid
+                items={filteredVehicles}
+            />
+        </Container>
+    </Layout>
 }
 export default vehiclesPage;
